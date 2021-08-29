8912b30f38fa4eab0549887c9595cccf499bac72
sdsparks11 <suberthi1998@gmail.com>
2021-08-29 08:35:59 +0530

    1 file modified, 1 added, 1 deleted

---
+++ src/app/productdummy.ts
@@ -0,0 +1,13 @@
+// JS array with objects...
+export const productdummy = [
+  {
+    'productId':1,
+    'productName':'A tale of two cities',
+    'price':200
+  },
+  {
+    'productId':2,
+    'productName':'Oliver Twist',
+    'price':300
+  }
+];
